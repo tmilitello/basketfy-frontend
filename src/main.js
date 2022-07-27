21414ad8a0266002bd40c9c0d0f964eceb0e199b
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-07-27 12:50:24 -0500

    changes fe main

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-// axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
-
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://basketfy-2022.herokuapp.com" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "basketfy-2022.herokuapp.com" : "/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
