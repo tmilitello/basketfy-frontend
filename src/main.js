28d6dcc11c4b7c6b28ddb1f5e885b438969e66ad
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-07-28 15:29:35 -0500

    adds stuff

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import axios from "axios";
 
-// axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://ancient-thicket-68427.herokuapp.com/" : "/";
-
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://basketfy-2022.herokuapp.com" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://basketfy-2022.herokuapp" : "/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
