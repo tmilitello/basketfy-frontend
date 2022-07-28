47ebb5638ab3fe83c48b235ebf0a3567341fc175
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-07-28 15:43:38 -0500

    adds axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://basketfy-2022.herokuapp" : "/";
 
